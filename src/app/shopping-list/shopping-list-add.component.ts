884cde01135cb5d4e5ea9cbf79e9962e4ff08dcb
Allen Whearry <awhearry@gmail.com>
2017-01-10 00:18:53 -0500

    Add template driven form CRUD for shopping list ingredients

--- src/app/shopping-list/shopping-list-add.component.ts
+++ src/app/shopping-list/shopping-list-add.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnChanges, Output, EventEmitter} from '@angular/core';
 import {Ingredient} from "../shared/ingredient";
 import {ShoppingListService} from "./shopping-list.service";
 
@@ -6,12 +6,42 @@ import {ShoppingListService} from "./shopping-list.service";
   selector: 'rb-shopping-list-add',
   templateUrl: './shopping-list-add.component.html',
 })
-export class ShoppingListAddComponent implements OnInit {
-  items: Ingredient[] = [];
+export class ShoppingListAddComponent implements OnChanges {
+  @Input() item: Ingredient;
+  @Output() cleared = new EventEmitter();
+  isAdd = true;
+
   constructor(private sls: ShoppingListService) { }
 
-  ngOnInit() {
-    this.items = this.sls.getItems();
+  ngOnChanges(changes) {
+    if (changes.item.currentValue === null) {
+      this.isAdd = true;
+      this.item = {name: null, amount: null}
+    } else {
+      this.isAdd = false;
+    }
   }
 
+
+  onSubmit(ingredient: Ingredient) {
+    const newIngredient = new Ingredient(ingredient.name, ingredient.amount);
+    if (!this.isAdd) {
+      this.sls.editItem(this.item, newIngredient );
+      this.onClear()
+    } else {
+      this.item = newIngredient;
+      this.sls.addItem(this.item);
+      this.onClear()
+    }
+  }
+
+  onDelete() {
+    this.sls.deleteItem(this.item);
+    this.onClear()
+  }
+
+  onClear(){
+    this.isAdd = true;
+    this.cleared.emit(null);
+  }
 }
